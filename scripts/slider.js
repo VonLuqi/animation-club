9b3bf4d6a4faf7abb0dca95317a51cb632fb1033
VonLuqi <vonluqi@gmail.com>
2026-02-05 23:34:48 -0300

    added depoiments sect

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,16 +1,30 @@
 const radios = document.querySelectorAll('.hero-slider-radio')
+const setas = document.querySelectorAll('.seta')
 
-const change_slide = (aux = 1) => {
-  const indexAtual = [...radios].findIndex(radio => radio.checked)
+const change_slide = (aux = 1, slider = null, norm = null, property = null, type = null) => {
+  if (!slider || !norm || !property || !type) return
 
-  const nextIndex = (indexAtual + aux + radios.length) % radios.length
+  let indexAtual = 0
+  let slide = null
 
-  radios[nextIndex].checked = true
-  document.querySelector('.hero_slider_container').style.setProperty('--index-slide', nextIndex)
+  if (type === 'radio') {
+    indexAtual = [...radios].findIndex(radio => radio.checked)
+  } else {
+    slide = slider.querySelectorAll('.wrapper_depoimento') 
+    indexAtual = [...slide].findIndex(s => s.classList.contains('active'))
+    slide[indexAtual].classList.remove('active')
+  }
+
+  const nextIndex = (indexAtual + aux + norm) % norm
+
+  if (slide && nextIndex < slide.length) slide[nextIndex].classList.add('active')
+
+  if (type === 'radio') radios[nextIndex].checked = true
+  slider.style.setProperty(`${property}`, nextIndex)
 }
 
 const intervalTime = 6500
-let interval = setInterval(change_slide, intervalTime)
+let interval = setInterval(() => {change_slide(1, document.querySelector('.hero_slider_container'), radios.length, '--index-slide', 'radio')}, intervalTime)
 
 const timeoutTime = 15000
 let timeout = null
@@ -20,7 +34,7 @@ const call_timeout = () => {
   clearTimeout(timeout)
 
   timeout = setTimeout(() => {
-    interval = setInterval(change_slide, intervalTime)
+    interval = setInterval(() => {change_slide(1, document.querySelector('.hero_slider_container'), radios.length, '--index-slide', 'radio')}, intervalTime)
   },timeoutTime)
 }
 
@@ -31,5 +45,16 @@ radios.forEach(radio => {
     const indexActual = e.target.dataset.index
 
     document.querySelector('.hero_slider_container').style.setProperty('--index-slide', indexActual)
+  })
+})
+
+setas.forEach(seta => {
+  seta.addEventListener('click', (e) => {
+    const side = e.currentTarget.dataset.side === 'next' ? 1 : -1
+    console.log(`side: ${side}`)
+    const slider = document.querySelector('.depoimentos')
+    const norm = slider.querySelectorAll('.wrapper_depoimento').length
+
+    change_slide(side, slider, norm, '--index-depoimento', 'seta')
   })
 })
